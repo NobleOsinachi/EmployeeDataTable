5ce618b94ed7a3812e969df19b058ebc002f2b36
Noble Osinachi <nobleosinachi98@gmail.com>
2023-09-05 22:50:03 +0100

    Upload the source files

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeDataTable.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeDataTable.Startup))]
+namespace EmployeeDataTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
